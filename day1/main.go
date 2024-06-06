c22a91211f3e699493bd679d1cf9b5aad6d90fd6
Iliyas Bekkhozha <iliyasbek2003@gmail.com>
2024-06-07 02:45:14 +0500

    first commit

---
+++ day1/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	// "io"
+	// "sort"
+	// "strings"
+	"strconv"
+	// "container/list"
+)
+
+const (
+	inf int = 1e18
+	mod int = 998244353
+	N   int = 500
+	M   int = 500
+)
+
+// func max(x, y int) int {
+// 	if x > y {
+// 		return x
+// 	}
+// 	return y
+// }
+
+// func min(x, y int) int {
+// 	if x < y {
+// 		return x
+// 	}
+// 	return y
+// }
+
+var (
+
+)
+
+func solve() {
+
+	var a []int
+	end := false
+	for !end {
+		line, err := r.ReadString('\n')
+		if err != nil && err.Error() == "EOF" {
+			end = true
+		}
+
+		n, _ := strconv.Atoi(strings.TrimSpace(line))
+		a = append(a, n)
+	}
+
+	res := 0
+	for _, n := range a {
+		n = n / 3 - 2
+		for n > 0 {
+			res += n
+			n = n / 3 - 2
+		}
+
+	}
+
+	fprintf("%d\n", res)
+
+}
+
+var (
+	w *bufio.Writer
+	r *bufio.Reader
+)
+
+func fscan(a ...any) {
+	fmt.Fscan(r, a...)
+}
+
+func fscanf(format string, a ...any) {
+	fmt.Fscanf(r, format, a...)
+}
+
+func fprintf(format string, a ...any) {
+	fmt.Fprintf(w, format, a...)
+}
+
+func main() {
+
+	// r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+
+	fin, _ := os.Open("f.in")
+	defer fin.Close()
+	r = bufio.NewReader(fin)
+
+	// fout, _ := os.Open("f.out")
+	// defer fout.Close()
+	// w = bufio.NewWriter(fout)
+
+	defer w.Flush()
+
+	tt := 1
+	// fscan(&tt)
+
+	for i := 0; i < tt; i++ {
+		solve()
+	}
+
+}
